e4d892440ee352a1970d2b552ab495e9066830b2
Soumik Mukherjee <smsoumik1608@gmail.com>
2021-01-01 03:04:04 +0530

    Program Added

---
+++ maxFrequency.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+#include<string>
+
+using namespace std;
+
+int main(){
+
+    string str;
+
+    cout<<"ENTER THE STRING OF LOWERCASE:";
+    cin >> str;
+
+    int freq[26];
+
+    for(int i=0; i<26; i++){
+        freq[i] =0;
+    }
+
+    for(int i = 0; i < str.size(); i++){
+        freq[str[i] - 'a']++;
+    }
+
+    int maxF = 0;
+    char ans = 'a';
+
+    for(int i = 0; i<26; i++){
+        if(freq[i] > maxF){
+            maxF = freq[i];
+            ans = i + 'a';
+        }
+    }
+
+    cout<<ans<<" has occurred "<<maxF<<" times."<<endl;
+    return 0;
+}
